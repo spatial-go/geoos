a6e07b5b2685c1cf88bd0f400503958050b4e256
rainstop_3 <zhyt1985@163.com>
2021-09-28 13:46:13 +0800

    fix coordtransform test bug

--- coordtransform/coordtransform_test.go
+++ coordtransform/coordtransform_test.go
@@ -1,8 +1,9 @@
 package coordtransform
 
 import (
-	"github.com/spatial-go/geoos/space"
 	"testing"
+
+	"github.com/spatial-go/geoos/algorithm/matrix"
 )
 
 func TestTransformer_TransformLatLng(t1 *testing.T) {
@@ -14,20 +15,20 @@ func TestTransformer_TransformLatLng(t1 *testing.T) {
 		lat float64
 	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   float64
-		want1  float64
+		name      string
+		fields    fields
+		args      args
+		want      float64
+		want1     float64
 		tolerance float64
 	}{
 		{
 			name: "lnglat to mercator", fields: fields{CoordType: LLTOMERCATOR},
-			args: args{lng: 110,lat:40}, want: 12245143.99, want1: 4865942.28, tolerance: 0.1,
+			args: args{lng: 110, lat: 40}, want: 12245143.99, want1: 4865942.28, tolerance: 0.1,
 		},
 		{
 			name: "mercator to lnglat", fields: fields{CoordType: MERCATORTOLL},
-			args: args{lng: 12245143,lat:4865942}, want: 109.9999911, want1: 39.9999981, tolerance: 0.0000001,
+			args: args{lng: 12245143, lat: 4865942}, want: 109.9999911, want1: 39.9999981, tolerance: 0.0000001,
 		},
 	}
 	for _, tt := range tests {
@@ -35,8 +36,8 @@ func TestTransformer_TransformLatLng(t1 *testing.T) {
 			t := GetInstance()
 			t.CoordType = tt.fields.CoordType
 			got, got1 := t.TransformLatLng(tt.args.lng, tt.args.lat)
-			gotPoint := space.Point{got, got1}
-			wantPoint := space.Point{tt.want, tt.want1}
+			gotPoint := matrix.Matrix{got, got1}
+			wantPoint := matrix.Matrix{tt.want, tt.want1}
 			if !gotPoint.EqualsExact(wantPoint, tt.tolerance) {
 				t1.Errorf("TransformLatLng() got = %v %v, want %v %v", got, got1, tt.want, tt.want1)
 			}
